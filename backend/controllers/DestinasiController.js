5b68aed542f968f5204641beaaf86627c288ab74
BISNISPRJCK <bisnisprojeck025@gmail.com>
2025-01-22 12:12:44 +0700

    menambahkan update dan getdata di controller destinasi (backend)

--- backend/controllers/DestinasiController.js
+++ backend/controllers/DestinasiController.js
@@ -47,3 +47,82 @@ export const createDestinasi = async(req, res)=>{
   });
 }
 
+
+export const getDestinasi = async(req, res)=>{
+  try {
+    const response = await Destinasi.findAll({
+      attributes: ['id', 'nama_gunung', 'deskripsi_gunung', 'foto_gunung'],
+
+    });
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+    
+  }
+}
+
+
+export const updateDestinasi = async(req, res)=> {
+  try {
+    const destinasi = await Destinasi.findOne({
+      where:{
+        id: req.params.id
+      }
+    });
+
+    if(!destinasi) res.status(404).json({msg: "Data Destinasi tidak ditemukan"});
+
+    let fileName;
+    if(!req.files || !req.files.foto_gunung){
+      fileName = Destinasi.foto_gunung;
+    }else{
+      const file = req.files.foto_gunung;
+      const fileSize = file.data.length;
+      const ext = path.extname(file.name);
+      fileName = file.md5 + ext ;
+      const allowedType = ['.png', '.jpg', '.jpeg'];
+
+      if(!allowedType.includes(ext.toLocaleLowerCase())) return res.status(422).json({msg: "Gambar Tidak Valid"});
+      if(fileSize > 5000000) return res.status(422).json({msg: "Gambar Harus Kurang dari 5 mb"});
+      const oldFilePath = `./public/images/destinasi/${destinasi.foto_gunung}`;
+      const newFilePath = `./public/images/destinasi/${fileName}`;
+
+      // Hapus file gambar lama
+      if(fs.existsSync(oldFilePath)){
+        fs.unlink(oldFilePath, (err)=>{
+          if(err) return res.status(500).json({msg: err.message});
+
+        });
+      }
+
+      // simpan file gambar baru 
+      file.mv(newFilePath, (err)=> {
+        if(err) return res.status(500).json({msg: err.message});
+      })
+    }
+    
+
+    const {nama_gunung, deskripsi_gunung, harga_gunung} = req.body;
+    
+
+
+    await Destinasi.update({
+      nama_gunung: nama_gunung,
+      deskripsi_gunung: deskripsi_gunung,
+      foto_gunung: fileName,
+      harga_gunung: harga_gunung
+    }, {
+      where:{
+        id: req.params.id
+      }
+    });
+
+    res.status(200).json({msg: "Data Berhasil Diperbarui"});
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({msg: "Kesalahan Server"})
+  }
+}
+
--- backend/routes/DestinasiRoutes.js
+++ backend/routes/DestinasiRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { createDestinasi } from "../controllers/DestinasiController.js";
+import { createDestinasi, getDestinasi, updateDestinasi } from "../controllers/DestinasiController.js";
 
 const route = express.Router();
 
 route.post('/createDestinasi', createDestinasi);
+route.put('/updateDestinasi/:id', updateDestinasi);
+route.get('/getDestinasi', getDestinasi);
 
 
 
