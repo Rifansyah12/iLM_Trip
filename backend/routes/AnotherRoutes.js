dbec731bdc4fcaec780b9717f6a79f1260737daf
RIFANSYAH <syahr9610@gmail.com>
2025-02-21 07:26:44 +0700

    finish project

--- backend/routes/AnotherRoutes.js
+++ backend/routes/AnotherRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import {
   createAnothertrip,
@@ -15,18 +14,5 @@ route.get("/getAnothertrip", getAnothertrip);
 route.get("/getAnothertripById/:id", getAnothertripById);
 route.put("/updateAnothertrip/:id", updateAnothertrip);
 route.delete("/deleteAnothertrip/:id", deleteAnothertrip);
-=======
-import express from 'express';
-import { createAnothertrip, getAnothertrip, updateAnothertrip, deleteAnothertrip, getAnothertripById } from '../controllers/AnothertripController.js';
-
-
-const route = express.Router();
-
-route.post('/createAnothertrip', createAnothertrip);
-route.get('/getAnothertrip', getAnothertrip)
-route.get('/getAnothertripById/:id', getAnothertripById)
-route.put('/updateAnothertrip/:id', updateAnothertrip)
-route.delete('/deleteAnothertrip/:id', deleteAnothertrip);
->>>>>>> 669af1f89099e17be35d75fa90fe6a9f9d2bf573
 
 export default route;
